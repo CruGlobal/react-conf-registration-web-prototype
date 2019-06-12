518cf2e3d4b94606cddeafdd498bdfb678f409df
TheNoodleMoose <cjh605@gmail.com>
2019-06-12 14:39:07 -0400

    Moved dropdownbutton to its own component

--- src/Components/LandingPageComponents/Navbar/Navbar.js
+++ src/Components/LandingPageComponents/Navbar/Navbar.js
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import {
-  faUsers,
-  faMedkit,
-  faSignOutAlt
-} from "@fortawesome/free-solid-svg-icons";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Dropdown from "react-bootstrap/Dropdown";
-import Modal from "react-bootstrap/Modal";
-import RelayImg from "../../../img/signin-relay.png";
-import FacebookImg from "../../../img/signin-facebook.png";
-import InstagramImg from "../../../img/signin-instagram.jpeg";
 import { Link } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
 import { connect } from "react-redux";
 import { userLogout } from "../../../actions";
 import LoginModal from "./Subcomponents/LoginModal";
+import DropDownButton from "./Subcomponents/DropdownButton";
 
 const Navbar = ({ profile, loginState, userLogout }) => {
   const DashboardButton = styled.button`
@@ -49,39 +39,7 @@ const Navbar = ({ profile, loginState, userLogout }) => {
               <DashboardButton data-testid="signed-in-title">
                 Hello <strong>{profile.firstName}</strong>
               </DashboardButton>
-              <Dropdown as={ButtonGroup}>
-                <DownArrowButton
-                  split
-                  id="dropdown-split-basic"
-                  data-testid="drop-down-button"
-                />
-
-                <Dropdown.Menu alignRight={true}>
-                  <LinkContainer to="/eventDashboard">
-                    <Dropdown.Item>
-                      <FontAwesomeIcon icon={faUsers} />
-                      <DropDownItemText>My Dashboard</DropDownItemText>
-                    </Dropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to="/help">
-                    <Dropdown.Item>
-                      <FontAwesomeIcon icon={faMedkit} />
-                      <DropDownItemText>I Need Help!</DropDownItemText>
-                    </Dropdown.Item>
-                  </LinkContainer>
-
-                  <Dropdown.Item>
-                    {" "}
-                    <FontAwesomeIcon icon={faSignOutAlt} />
-                    <DropDownItemText
-                      onClick={userLogout}
-                      data-testid="sign-out-title"
-                    >
-                      Sign out
-                    </DropDownItemText>
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
+              <DropDownButton userLogout={userLogout} />
             </>
           ) : (
             <>
@@ -139,6 +97,12 @@ const NavBar = styled.nav`
   margin-bottom: 5px;
 `;
 
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
 const MainHeadingContainer = styled(Link)`
   display: flex;
   flex-direction: row;
@@ -154,26 +118,4 @@ const NavBarTitle = styled.h3`
   font-size: 36px;
   text-decoration: none;
   margin: 0 10px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 15px;
-`;
-
-const DownArrowButton = styled(Dropdown.Toggle)`
-  height: 36px;
-  width: 30px;
-  border-top-right-radius: 5px;
-  border-bottom-right-radius: 5px;
-  border-top-left-radius: 0px;
-  border-bottom-left-radius: 0px;
-  background-image: linear-gradient(to bottom, #3494c7 0px, #2b86b7 100%);
-  border-color #2969a0;
-  color: white;
-`;
-
-const DropDownItemText = styled.span`
-  margin-left: 5px;
 `;
