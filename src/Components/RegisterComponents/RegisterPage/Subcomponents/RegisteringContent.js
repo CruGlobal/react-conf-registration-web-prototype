146d0422919b3469b89d0e4bbc914bb74ebf2cc7
jammerwhy <david.yue@cru.org>
2019-06-14 15:50:55 -0400

    Imported ALL the components to the registraton page. (You're welcom, guys.)

--- src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
+++ src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
@@ -1,11 +1,35 @@
 import React from "react";
 import styled from "@emotion/styled";
-import Name from "../../QuestionsComponents/nameQuestion";
-import Email from "../../QuestionsComponents/emailQuestion";
+import AddressQuestion from "../../QuestionsComponents/addressQuestion";
+import CampusQuestion from "../../QuestionsComponents/campusQuestion";
+import CheckboxQuestion from "../../QuestionsComponents/checkboxQuestion";
+import DateQuestion from "../../QuestionsComponents/dateQuestion";
+import EmailQuestion from "../../QuestionsComponents/emailQuestion";
+import GenderQuestion from "../../QuestionsComponents/genderQuestion";
+import NameQuestion from "../../QuestionsComponents/nameQuestion";
+import PhoneQuestion from "../../QuestionsComponents/PhoneQuestion";
+import RadioQuestion from "../../QuestionsComponents/radioQuestion";
+import SelectQuestion from "../../QuestionsComponents/selectQuestion";
+import TextAreaQuestion from "../../QuestionsComponents/textAreaQuestion";
+import TextQuestion from "../../QuestionsComponents/textQuestion";
+import YearQuestion from "../../QuestionsComponents/yearQuestion";
+
 const RegisteringContent = () => {
   return (
     <div>
-      <Email />
+      <AddressQuestion />
+      <CampusQuestion />
+      <CheckboxQuestion />
+      <DateQuestion />
+      <EmailQuestion />
+      <GenderQuestion />
+      <NameQuestion />
+      <PhoneQuestion />
+      <RadioQuestion />
+      <SelectQuestion />
+      <TextAreaQuestion />
+      <TextQuestion />
+      <YearQuestion />
     </div>
   );
 };
