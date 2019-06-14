33932d737ae62505b8cbfc70b8d4bd6b22869bbd
Jizhou Zhang <Jzhang3@oakland.edu>
2019-06-14 15:15:17 -0400

    Email and Name

--- src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
+++ src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "@emotion/styled";
-import Checkbox from "../../QuestionsComponents/checkboxQuestion";
-
+import Name from "../../QuestionsComponents/nameQuestion";
+import Email from "../../QuestionsComponents/emailQuestion";
 const RegisteringContent = () => {
   return (
     <div>
-      <Checkbox />
+      <Email />
     </div>
   );
 };
