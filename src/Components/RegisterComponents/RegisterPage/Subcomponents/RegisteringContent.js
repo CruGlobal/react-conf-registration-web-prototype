500e16fa186d4c951caa59df5d8057e44e08ebfc
Gibbsface <cole.gibbs@gmail.com>
2019-06-14 13:54:31 -0400

    added yearQuestion and radioQuestion

--- src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
+++ src/Components/RegisterComponents/RegisterPage/Subcomponents/RegisteringContent.js
@@ -1,12 +1,16 @@
 import React from "react";
 import AddressQuestion from "../../QuestionsComponents/addressQuestion";
 import GenderQuestion from "../../QuestionsComponents/genderQuestion";
+import RadioQuestion from "../../QuestionsComponents/radioQuestion";
+import YearQuestion from "../../QuestionsComponents/yearQuestion";
 
 const RegisteringContent = () => {
   return (
     <>
       <AddressQuestion />
       <GenderQuestion />
+      <RadioQuestion />
+      <YearQuestion />
     </>
   );
 };
