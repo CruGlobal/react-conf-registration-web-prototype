a4bfd1df98dfc0c286cf40b6f56aa78ee2368e9c
TheNoodleMoose <cjh605@gmail.com>
2019-07-09 10:42:08 -0400

    Fixed continue button to ignore empty pages and only go to a page that has questions

--- src/Components/RegisterComponents/RegisterPage/Subcomponents/ContinueButton.js
+++ src/Components/RegisterComponents/RegisterPage/Subcomponents/ContinueButton.js
@@ -5,18 +5,30 @@ import { connect } from "react-redux";
 const ContinueButton = ({ match, conference, history, isSaving }) => {
   // Create a variable with an empty value, will be used to set the route we wish to push too
   let nextPage = "";
-  // Filter through our pages to find the current page we are on
-  const currentPage = conference.registrationPages.filter(
+
+  // This function takes in the conference and returns an array of all the pages that have blocks with content in them
+  const CalculateIfPageHasBlocks = conference => {
+    let conferencesWithBlocks = conference.registrationPages.filter(
+      Page => Page.blocks.length > 0
+    );
+
+    return conferencesWithBlocks;
+  };
+  // Create a variable that we will use to calculate whether a page has a next page or not
+  let PagesWithBlocks = CalculateIfPageHasBlocks(conference);
+
+  // Filter through our pages that have content in their blocks to find the current page we are on
+  const currentPage = PagesWithBlocks.filter(
     Pages => Pages.id === match.params.pageID
   );
   // Find the index number we are currently on
-  const CurrentPageIndex = conference.registrationPages.indexOf(currentPage[0]);
+  const CurrentPageIndex = PagesWithBlocks.indexOf(currentPage[0]);
 
   // If the index of current page + 1 does not equal the total length of all the pages, then it has a next page
   const HasNextPage = () => {
     if (
-      conference.registrationPages.indexOf(currentPage[0]) + 1 !==
-      conference.registrationPages.length
+      PagesWithBlocks.indexOf(currentPage[0]) + 1 !==
+      PagesWithBlocks.length
     ) {
       return true;
     } else {
@@ -27,7 +39,7 @@ const ContinueButton = ({ match, conference, history, isSaving }) => {
   // We use this to calculate which page we want to route to and what text to render
   const CalculatePage = () => {
     // If the amount of pages is greater than one and is not currently saving check if it has a next page
-    if (conference.registrationPages.length > 1 && !isSaving) {
+    if (PagesWithBlocks.length > 1 && !isSaving) {
       if (HasNextPage()) {
         nextPage = `/register/${match.params.confID}/page/${
           conference.registrationPages[CurrentPageIndex + 1].id
